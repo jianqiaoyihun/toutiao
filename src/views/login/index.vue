8772ab47d8c82c8366e31967d91bbffe14f2ba11
wuwangxing <15028597033@163.com>
2019-12-18 20:43:31 +0800

    黑马头条PC-登录模块-数据绑定及校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,19 +2,19 @@
     <div class='login'>
         <el-card class='login-card'>
             <div class="title"><img src="../../assets/img/logo_index.png" alt=""></div>
-            <el-form style="margin-top:30px">
-                <el-form-item>
-                    <el-input placeholder="请输入手机号"></el-input>
-                </el-form-item>
-                <el-form-item>
-                    <el-input style="width:65%"></el-input>
+            <el-form style="margin-top:30px" :model="loginForm" :rules="loginRules" ref="myForm">
+                <el-form-item prop="mobile">
+                    <el-input placeholder="请输入手机号" v-model="loginForm.mobile"></el-input>
+                </el-form-item >
+                <el-form-item prop="code">
+                    <el-input style="width:65%" v-model="loginForm.code"></el-input>
                     <el-button plain style="float:right">发送验证码</el-button>
                 </el-form-item>
-                <el-form-item>
-                    <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
+                <el-form-item prop="check">
+                    <el-checkbox v-model="loginForm.check">我已阅读并同意用户协议和隐私条款</el-checkbox>
                 </el-form-item>
                 <el-form-item>
-                    <el-button type="primary" style="width:100%">登录</el-button>
+                    <el-button type="primary" style="width:100%" @click="submitLogin">登录</el-button>
                 </el-form-item>
             </el-form>
         </el-card>
@@ -23,7 +23,31 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      loginForm: {
+        mobile: '',
+        code: '',
+        check: false
+      },
+      loginRules: {
+        mobile: [{ required: true, message: '请输入您的手机号' }, { pattern: /^1[3456789]\d{9}$/, message: '请输入合法的手机号' }],
+        code: [{ required: true, message: '请输入验证码' }, { pattern: /^\d{6}$/, message: '请输入正确的验证码' }],
+        check: [{ validator: function (rule, value, callback) {
+          value ? callback() : callback(new Error('您必须统一策略'))
+        } }]
+      }
+    }
+  },
+  methods: {
+    submitLogin () {
+      this.$refs.myForm.validate(function (isOk) {
+        if (isOk) {
+          console.log('dengluchenggong')
+        }
+      })
+    }
+  }
 }
 </script>
 

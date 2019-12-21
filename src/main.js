ad8031e3a58a62e4385d108190388e9778d9c65e
wuwangxing <15028597033@163.com>
2019-12-21 16:29:41 +0800

    黑马头条PC-接口访问-axios拦截器-统一处理请求token

--- src/main.js
+++ src/main.js
@@ -7,8 +7,8 @@ import ElementUI from 'element-ui'
 import Component from './components/index.js'
 import 'element-ui/lib/theme-chalk/index.css'
 import './styles/index.less' // 引入样式
-import axios from 'axios'
-axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
+import axios from './utils/request.js'
+
 Vue.prototype.$axios = axios
 
 Vue.config.productionTip = false

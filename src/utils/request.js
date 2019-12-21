774c0ef4c31e49c477f48e04784fb3542f5b4a6e
wuwangxing <15028597033@163.com>
2019-12-21 16:41:03 +0800

    黑马头条PC--接口访问-axios拦截器-统一处理响应数据

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
+
+// axios 请求拦截注入headers  token令牌
 axios.interceptors.request.use(function (config) {
   let token = window.localStorage.getItem('user-token')
   config.headers['Authorization'] = `Bearer ${token}`
@@ -8,4 +10,10 @@ axios.interceptors.request.use(function (config) {
   return Promise.reject(error)
 })
 
+// axios 响应拦截，过滤数据
+axios.interceptors.response.use(function (response) {
+  return response.data ? response.data : {}
+}, function () {
+
+})
 export default axios

aa0222ab22f3ffd84ae6278ce1788453a8c839b7
wuwangxing <15028597033@163.com>
2019-12-24 23:45:57 +0800

    黑马头条PC-内容列表-分页请求-删除内容

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -15,7 +15,8 @@
               0-草稿，1-待审核，2-审核通过，3-审核失败，4-已删除，不传为全部
         -->
         <!-- 单选框组 -->
-        <el-radio-group v-model="formdata.status" style="height:40px;line-height:50px">
+        <!-- {{formdata.status}} -->
+        <el-radio-group v-model="formdata.status" style="height:40px;line-height:50px" @change="changeCondition">
           <el-radio :label="5">全部</el-radio>
           <el-radio :label="0">草稿</el-radio>
           <el-radio :label="1">待审核</el-radio>
@@ -31,7 +32,8 @@
       </el-col>
       <el-col :span="16">
         <!-- 下拉菜单 -->
-        <el-select v-model="formdata.channel_id" placeholder="请选择">
+        <!-- {{formdata.channel_id}} -->
+        <el-select v-model="formdata.channel_id" placeholder="请选择" @change="changeCondition">
           <el-option
             v-for="item in channelsList"
             :key="item.id"
@@ -50,6 +52,8 @@
         <!-- 时间 -->
         <div class="block">
           <el-date-picker
+            @change="changeCondition"
+            value-format="yyyy-MM-dd"
             v-model="formdata.dateRange"
             type="daterange"
             range-separator="-"
@@ -58,11 +62,12 @@
           ></el-date-picker>
         </div>
       </el-col>
+      <!-- {{formdata.dateRange.length}} -->
     </el-row>
   </el-card>
   <el-card class="articleCard">
     <el-row class="total">
-      <span>共找到62282条符合条件的内容</span>
+      <span>共找到{{page.total}}条符合条件的内容</span>
     </el-row>
      <el-row  v-for="item in articlesList" :key="item.id.toString()" class='article-item' type='flex' justify="space-between">
         <!-- 左侧 -->
@@ -70,7 +75,7 @@
            <el-row type='flex'>
              <img :src=" item.cover.images.length ? item.cover.images : defaultImage " alt="">
               <div class='info'>
-                <span style="">{{ item.title }}</span>
+                <span>{{ item.title }}</span>
                 <!-- 过滤器不但可以在插值表达中使用 还可以在v-bind表达式中使用 -->
                 <el-tag class='tag' :type=" item.status | filterType ">{{ item.status | filterStatus }}</el-tag>
                 <span class='date'>{{ item.pubdate }}</span>
@@ -81,9 +86,21 @@
        <el-col :span="6">
            <el-row class='right' type='flex' justify="end">
                <span><i class="el-icon-edit"></i>修改</span>
-               <span><i class="el-icon-delete"></i> 删除</span>
+               <span @click="delArticle(item.id)"><i class="el-icon-delete"></i> 删除</span>
            </el-row>
        </el-col>
+    </el-row>
+    <!-- 分页 -->
+    <el-row type='flex' justify="center" align="middle" style='height:60px'>
+      <el-pagination
+        background
+        layout="prev, pager, next"
+        :total="page.total"
+        :current-page="page.currentPage"
+        :page-size="page.pageSize"
+        @current-change="changePage"
+        >
+        </el-pagination>
     </el-row>
   </el-card>
 </div>
@@ -97,11 +114,16 @@ export default {
       formdata: {
         status: 5,
         channel_id: null,
-        dateRange: null
+        dateRange: []
       },
       defaultImage: require('../../assets/img/404.png'),
       channelsList: [],
-      articlesList: []
+      articlesList: [],
+      page: {
+        total: 0,
+        currentPage: 1,
+        pageSize: 10
+      }
     }
   },
   methods: {
@@ -112,12 +134,50 @@ export default {
         this.channelsList = result.data.channels
       })
     },
-    getArticles () {
+    getArticles (params) {
       this.$axios({
-        url: '/articles'
+        url: '/articles',
+        params
       }).then(result => {
         console.log(result)
         this.articlesList = result.data.results
+        this.page.total = result.data.total_count // 文章总数
+      })
+    },
+    changeCondition () {
+      this.page.currentPage = 1
+      this.getConditionArticle()
+    },
+    changePage (newPage) {
+      this.page.currentPage = newPage
+      this.getConditionArticle()
+    },
+    getConditionArticle () {
+      if (this.formdata.dateRange === null) {
+        this.formdata.dateRange = []
+      }
+      let params = {
+        page: this.page.currentPage, // 分页信息
+        per_page: this.page.pageSize, // 分页信息
+        status: this.formdata.status === 5 ? null : this.formdata.status,
+        channel_id: this.formdata.channel_id,
+        begin_pubdate: this.formdata.dateRange.length ? this.formdata.dateRange[0] : '',
+        end_pubdate: this.formdata.dateRange.length > 1 ? this.formdata.dateRange[1] : null
+      }
+      this.getArticles(params)
+    },
+    delArticle (id) {
+      this.$confirm('您确定要删除这篇文章吗？').then(() => {
+        this.$axios({
+          url: `/articles/${id.toString()}`,
+          method: 'delete'
+        }).then(() => {
+          this.$message({
+            type: 'success',
+            message: '删除文章成功'
+          })
+          this.getConditionArticle()
+        })
       })
     }
   },
@@ -155,7 +215,7 @@ export default {
   },
   created () {
     this.getChannels()
-    this.getArticles()
+    this.getArticles({ page: 1, per_page: 10 })
   }
 }
 </script>

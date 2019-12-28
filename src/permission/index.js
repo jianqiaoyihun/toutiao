8161c04fdfbe0f190a957e0b10fff8932d253780
wuwangxing <15028597033@163.com>
2019-12-28 20:32:31 +0800

    黑马头条PC-页面进度条引入应用

--- src/permission/index.js
+++ src/permission/index.js
@@ -1,8 +1,11 @@
 import router from '../router'
+import progress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 router.beforeEach(function (to, from, next) {
   // console.log(to)
   // let res = to.path.startsWith('/home')
+  progress.start()
   if (to.path.startsWith('/home')) {
     let token = window.localStorage.getItem('user-token')
     if (token) {
@@ -14,5 +17,7 @@ router.beforeEach(function (to, from, next) {
     next()
   }
 })
-
+router.afterEach(() => {
+  progress.done()
+})
 export default router

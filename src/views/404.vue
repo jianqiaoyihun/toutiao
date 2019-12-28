e88878ad944882d2f599dfa4d277c7223c81d0dd
wuwangxing <15028597033@163.com>
2019-12-28 20:01:47 +0800

    黑马头条PC-404页面应用

---
+++ src/views/404.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class='not-found'>
+       <img src="../assets/img/404.png" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang='less' scoped>
+  .not-found {
+      height: 100vh;
+      display: flex;
+      justify-content: center;
+      align-items:center;
+      img {
+        width: 100%;
+        height: 100%
+      }
+  }
+</style>
